ebd5f6f1262fe0b42912a0c9afe27415a3d896a5
ishween <ishweensehmbhi@gmail.com>
2022-03-18 11:32:24 -0400

    create DisplayGame component

---
+++ src/DisplayGame.js
@@ -0,0 +1,7 @@
+import { useEffect } from "react";
+
+DisplayGame = () => {
+	// make an API call to the PokemonAPI to determine the Pokemon assigned to the player
+};
+
+export default DisplayGame;
