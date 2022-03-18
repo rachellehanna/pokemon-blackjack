ca11270bd7e99fe4bb04a8996277a402653cddbd
shradha <mehta_shradha@yahoo.com>
2022-03-18 11:37:43 -0400

    Added Footer file

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+//Footer.js
+
+const Header = () => {
+  return (
+    <Footer>
+      <p>Created at Juno College by Allison, Ishween Sehmbhi, Rachelle Hanna, Shradha Mehta</p>
+    </Footer>
+  )
+}
+
+
+export default Footer;
