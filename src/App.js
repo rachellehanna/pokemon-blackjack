62927ebf07500a5987397951cd68c54f259d4340
Rachelle Hanna <ha.rachelle@gmail.com>
2022-03-19 17:08:57 -0300

    Add basic blackjack logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import DisplayGame from "./DisplayGame";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <DisplayGame />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
