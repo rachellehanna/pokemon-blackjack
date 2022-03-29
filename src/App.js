42281c7a862312c7d6650ebbaa53264e78a86b8b
Rachelle Hanna <ha.rachelle@gmail.com>
2022-03-29 14:21:00 -0300

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import Modal from "react-modal";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import DisplayGame from "./components/DisplayGame";
 
@@ -19,13 +18,12 @@ function App() {
                 className="modal-content"
                 ariaHideApp={false}
             >
-                <h1 className="modal-title">Hello Friends !</h1>
+                <h1>
+                    <span className="header-pokemon">Pokemon</span> <br />
+                    <span className="header-blackjack">Blackjack</span>
+                </h1>
 
                 <div className="welcome">
-                    <p>
-                        This is Pokemon Blackjack where two players can battle
-                        with cards.
-                    </p>
                     <p>
                         The player with a card total closest to 21 wins, but if
                         you go over 21, you lose. Win the game to evolve your
@@ -43,7 +41,6 @@ function App() {
             </Modal>
 
             <div className="wrapper">
-                <Header />
                 <DisplayGame />
             </div>
             <Footer />
